afc8444633191624cd275b289dede45ccf348e9f
Johannes Baron <johannes.baron3012@gmail.com>
2023-11-06 20:23:41 +0100

    Extrarückzahlungen hinzugefügt

--- Schulden Tilgung (HA 2.11.2023).py
+++ Schulden Tilgung (HA 2.11.2023).py
@@ -2,8 +2,10 @@
 Schulden = float(input("Schulden in Euro zu Beginn: "))
 Zinssatz = float(input("Zinssatz in Prozent: "))
 Tilgung = float(input("Tilgung in Euro pro Monat: "))
+Extrarükzahlungen = float(input("Extrarückzahlunge in Euro (gesamt): "))
 zins = Zinssatz / 100
 # Verarbeitung
+Schulden = Schulden - Extrarükzahlungen
 jahr = 0
 while Schulden > 0:
     Schulden = (Schulden + zins)  - (Tilgung * 12)
