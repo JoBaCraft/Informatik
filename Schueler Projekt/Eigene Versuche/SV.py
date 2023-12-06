790baa7b811968a3f35f1484f4592083251e909d
Johannes Baron <johannes.baron3012@gmail.com>
2023-12-06 16:48:47 +0100

    changes in funktion

--- Schueler Projekt/Eigene Versuche/SV.py
+++ Schueler Projekt/Eigene Versuche/SV.py
@@ -31,24 +31,28 @@ while AnzahlSchiffe > VorhandeneSchiffe:
     if feld[SchiffZeile][SchiffSpalte] != "S":
         feld[SchiffZeile][SchiffSpalte] = "S"
         VorhandeneSchiffe += 1
+    if feld[SchiffZeile][SchiffSpalte] != "S":
+        if feld[SchiffZeile-1][SchiffSpalte] != "S" or feld[SchiffZeile-1][SchiffSpalte] >= 7:
+           if feld[SchiffZeile-1][SchiffSpalte-1] != "S" or feld[SchiffZeile-1][SchiffSpalte-1] >= 7:
+                if feld[SchiffZeile][SchiffSpalte-1] != "S" or feld[SchiffZeile][SchiffSpalte-1] >= 7:
+                    if feld[SchiffZeile+1][SchiffSpalte-1] != "S" or feld[SchiffZeile+1][SchiffSpalte-1] >= 7:
+                        if feld[SchiffZeile+1][SchiffSpalte] != "S" or feld[SchiffZeile+1][SchiffSpalte] >= 7:
+                            if feld[SchiffZeile+1][SchiffSpalte+1] != "S" or feld[SchiffZeile+1][SchiffSpalte+1] >= 7:
+                                if feld[SchiffZeile][SchiffSpalte+1] != "S" or feld[SchiffZeile][SchiffSpalte+1] >= 7:
+                                    if feld[SchiffZeile-1][SchiffSpalte+1] != "S" or feld[SchiffZeile-1][SchiffSpalte+1] >= 7:
 
 print("Computersicht:")
 zeigeComputersicht(feld)
     
 #Beschuss
 
-#Achtung Fehler
-for i, textstelle in sum(feld, []) == "S":
-#Endet hier
-    
-    zeile = int(input("Zeile: "))
-    spalte = int(input("Spalte: "))
+zeile = int(input("Zeile: "))
+spalte = int(input("Spalte: "))
+if feld[zeile - 1][spalte - 1] == "S":
+    feld[zeile - 1][spalte - 1] = "T"
 
-    if feld[zeile - 1][spalte - 1] == "S":
-        feld[zeile - 1][spalte - 1] = "T"
-    
-    elif feld[zeile - 1][spalte - 1] == "~":
-        feld[zeile - 1][spalte - 1] = "X"
-    
-    print("Spielersicht:")
-    zeigeSpielersicht(feld)
+elif feld[zeile - 1][spalte - 1] == "~":
+    feld[zeile - 1][spalte - 1] = "X"
+
+print("Spielersicht:")
+zeigeSpielersicht(feld)
