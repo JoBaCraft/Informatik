01d3da8caa717c0a4a26f948aa572195fc1686cd
JoBaCraft <johannes.baron3012@gmail.com>
2024-05-29 20:17:21 +0200

    Update Schulden Tilgung (HA 2.11.2023).py

--- Hausaufgaben/Schulden Tilgung (HA 2.11.2023).py
+++ Hausaufgaben/Schulden Tilgung (HA 2.11.2023).py
@@ -3,13 +3,13 @@ Schulden = float(input("Schulden in Euro zu Beginn: "))
 Zinssatz = float(input("Zinssatz in Prozent: "))
 Tilgung = float(input("Tilgung in Euro pro Monat: "))
 Extrarükzahlungen = float(input("Extrarückzahlunge in Euro (gesamt): "))
-zins = Zinssatz / 100
+zins = (Zinssatz / 100) + 1
 # Verarbeitung
 Schulden = Schulden - Extrarükzahlungen
 jahr = 0
 while Schulden > 0:
-    Schulden = (Schulden + zins)  - (Tilgung * 12)
+    Schulden = (Schulden * zins)  - (Tilgung * 12)
     jahr += 1
 
 # Ausgabe
-print("Du brauchst ", jahr,"Jahre um deine Schulden zu Tilgen.") 
+print(f"Du brauchst {jahr} Jahre um deine Schulden zu Tilgen.") 
